ce775b2d4a1abee2570ded93583306023b0b1fce
Bichitra Gautam <bichitragautam25@gmail.com>
2025-01-07 21:35:27 +0545

    Add image gallery component with photo fetching and display functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,114 +1,151 @@
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import Head from "next/head";
+import { useMemo } from "react";
+import { GetStaticProps } from "next";
+import nodeFetch from "node-fetch";
+import { createApi } from "unsplash-js";
+import classNames from "classnames";
+import bgImage from "@/../public/photography-bg.jpeg";
+import Link from "next/link";
+import { getImages } from "@/utils/image-util";
+import { Photo } from "@/types/types";
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
+import { Gallery } from "@/components/gallery/Gallery";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
-export default function Home() {
+const tabs = [
+  {
+    key: "all",
+    display: "All",
+  },
+  {
+    key: "oceans",
+    display: "Oceans",
+  },
+  {
+    key: "forests",
+    display: "Forests",
+  },
+];
+
+type HomeProps = {
+  oceans: Photo[];
+  forests: Photo[];
+};
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const unsplash = createApi({
+    accessKey: process.env.UNSPLASH_ACCESS_KEY!,
+    fetch: nodeFetch as unknown as typeof fetch,
+  });
+
+  const [oceans, forests] = await Promise.all([
+    getImages(unsplash, "oceans"),
+    getImages(unsplash, "forests"),
+  ]);
+
+  return {
+    props: {
+      oceans,
+      forests,
+    },
+    // revalidate: 10,    uncomment for ISR
+  };
+};
+
+export default function Home({ oceans, forests }: HomeProps) {
+  const allPhotos = useMemo(() => {
+    const all = [...oceans, ...forests];
+
+    return all.sort((a, b) => b.likes - a.likes);
+  }, [oceans, forests]);
+  
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      {/* Head component */}
+      <Head>
+        <title>photography Website</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charSet="UTF-8" />
+        <meta name="author" content="Bichitra Gautam" />
+        <meta name="description" content="dynamic photography web app" />
+        <meta name="keywords" content="photography, web app, dynamic" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      {/* Background */}
+
+      <Image
+        priority
+        className="fixed left-0 top-0 z-0"
+        src={bgImage}
+        alt="background-image"
+        placeholder="blur"
+      />
+
+      {/* Content */}
+
+      <div className="fixed left-0 top-0 w-full h-full z-10 from-stone-900 bg-gradient-to-t"></div>
+
+      {/* Header */}
+
+      <header className="fixed top-0 w-full z-30 flex justify-between items-center h-[90px] px-10">
+        <span className="uppercase text-lg font-medium">
+          Photography Website
+        </span>
+        <Link
+          href="#"
+          className="rounded-3xl bg-white text-stone-700 px-3 py-2 hover:bg-opacity-90"
+        >
+          Get in touch
+        </Link>
+      </header>
+
+      {/* main */}
+
+      <main className="relative pt-[110px] z-20">
+        <div className="flex flex-col items-center h-full">
+          <TabGroup>
+            <TabList className="flex items-center gap-12">
+              {tabs.map((tab) => (
+                <Tab key={tab.key} className="p-2">
+                  {({ selected }) => (
+                    <span
+                      className={classNames(
+                        "uppercase text-lg",
+                        selected ? "text-white" : "text-stone-600"
+                      )}
+                    >
+                      {tab.display}
+                    </span>
+                  )}
+                </Tab>
+              ))}
+            </TabList>
+            <TabPanels className="h-full max-w-[900px] w-full p-2 sm:p-4 my-6">
+              <TabPanel className="overflow-auto">
+                <Gallery photos={allPhotos} />
+              </TabPanel>
+              <TabPanel>
+                <Gallery photos={oceans} />
+              </TabPanel>
+              <TabPanel>
+                <Gallery photos={forests} />
+              </TabPanel>
+            </TabPanels>
+          </TabGroup>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      {/* Footer */}
+      
+      <footer className="relative h-[90px] flex justify-center items-center uppercase text-lg font-medium z-20">
+        <p>Photography portfolio</p>
       </footer>
+
+      
+
     </div>
   );
 }
